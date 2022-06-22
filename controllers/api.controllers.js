2ba15783ed0fbae5bc0bbdbce16a891a69fa110d
Simião Guevan Jr <guevanjr@users.noreply.github.com>
2022-06-22 11:25:24 +0200

    Test Push API Option 1 - Receive Payment

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -55,7 +55,7 @@ exports.receivePayment = async function(req, res) {
                 var response = '';
                 parseString(req.body, function (err, result) {
                     response = result;
-                    res.send(response["Body"]);
+                    res.send(JSON.parse(response));
                 });
                 //var response = xmlHandler.xmlToJson(null, req.body, null);
             }
@@ -63,6 +63,50 @@ exports.receivePayment = async function(req, res) {
 };
 
 exports.sendPayment = async function(req, res) {
+    var phoneNumber = '861401090';
+    var messageText = 'Test e-Mola API';
+    var amount = 1;
+    var transId = '0123456789';
+
+    var reqText = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' " +
+        "xmlns:web='http://webservice.bccsgw.viettel.com/'>" +
+        "<soapenv:Header/>" +
+        "<soapenv:Body>" +
+        "<web:gwOperation>" +
+        "<Input>" +
+        "<username>" + userName + "</username>" +
+        "<password>" + passWord + "</password>" +
+        "<wscode>pushUssdDisbursementB2C</wscode>" +
+        "<param name='partnerCode' value='" + partnerCode + "'/>" +
+        "<param name='msisdn' value='" + phoneNumber + "'/>" +
+        "<param name='smsContent' value='" + messageText + "'/>" +
+        "<param name='transAmount' value='" + amount + "'/>" +
+        "<param name='transId' value='" + transId + "'/>" +
+        "<param name='key' value='" + key + "'/>" +
+        "<rawData>?</rawData>" +
+        "</Input>" +
+        "</web:gwOperation>" +
+        "</soapenv:Body>" +
+        "</soapenv:Envelope>";
+
+        request.post({
+            headers: {'content-type': 'text/xml;charset=UTF-8'},
+            url:     apiUrl,
+            body:    reqText
+        }, function(error, resp, body){
+            if (error) {
+                console.log('*** ERROR ***\n ' + error + '\n *** END ERROR ***');
+                res.send(error);
+            } else {
+                console.log('*** SUCCESS ***\n' + resp.body + '\n*** END SUCCESS ***');
+                var response = '';
+                parseString(req.body, function (err, result) {
+                    response = result;
+                    res.send(JSON.parse(response));
+                });
+                //var response = xmlHandler.xmlToJson(null, req.body, null);
+            }
+        });
 
 };
 
@@ -71,6 +115,44 @@ exports.paymentStatus = async function(req, res) {
 };
 
 exports.accountName = async function(req, res) {
+    var phoneNumber = '861401090';
+
+    var reqText = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' " +
+        "xmlns:web='http://webservice.bccsgw.viettel.com/'>" +
+        "<soapenv:Header/>" +
+        "<soapenv:Body>" +
+        "<web:gwOperation>" +
+        "<Input>" +
+        "<username>" + userName + "</username>" +
+        "<password>" + passWord + "</password>" +
+        "<wscode>queryBeneficiaryName</wscode>" +
+        "<param name='partnerCode' value='" + partnerCode + "'/>" +
+        "<param name='msisdn' value='" + phoneNumber + "'/>" +
+        "<param name='key' value='" + key + "'/>" +
+        "<rawData>?</rawData>" +
+        "</Input>" +
+        "</web:gwOperation>" +
+        "</soapenv:Body>" +
+        "</soapenv:Envelope>";
+
+        request.post({
+            headers: {'content-type': 'text/xml;charset=UTF-8'},
+            url:     apiUrl,
+            body:    reqText
+        }, function(error, resp, body){
+            if (error) {
+                console.log('*** ERROR ***\n ' + error + '\n *** END ERROR ***');
+                res.send(error);
+            } else {
+                console.log('*** SUCCESS ***\n' + resp.body + '\n*** END SUCCESS ***');
+                var response = '';
+                parseString(req.body, function (err, result) {
+                    response = result;
+                    res.send(JSON.parse(response));
+                });
+                //var response = xmlHandler.xmlToJson(null, req.body, null);
+            }
+        });
 
 };
 
@@ -79,5 +161,43 @@ exports.approvalStatus = async function(req, res) {
 };
 
 exports.getBalance = async function(req, res) {
+    var transId = '0123456789';
+
+    var reqText = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' " +
+        "xmlns:web='http://webservice.bccsgw.viettel.com/'>" +
+        "<soapenv:Header/>" +
+        "<soapenv:Body>" +
+        "<web:gwOperation>" +
+        "<Input>" +
+        "<username>" + userName + "</username>" +
+        "<password>" + passWord + "</password>" +
+        "<wscode>queryAccountBalance</wscode>" +
+        "<param name='partnerCode' value='" + partnerCode + "'/>" +
+        "<param name='transId' value='" + transId + "'/>" +
+        "<param name='key' value='" + key + "'/>" +
+        "<rawData>?</rawData>" +
+        "</Input>" +
+        "</web:gwOperation>" +
+        "</soapenv:Body>" +
+        "</soapenv:Envelope>";
+
+        request.post({
+            headers: {'content-type': 'text/xml;charset=UTF-8'},
+            url:     apiUrl,
+            body:    reqText
+        }, function(error, resp, body){
+            if (error) {
+                console.log('*** ERROR ***\n ' + error + '\n *** END ERROR ***');
+                res.send(error);
+            } else {
+                console.log('*** SUCCESS ***\n' + resp.body + '\n*** END SUCCESS ***');
+                var response = '';
+                parseString(req.body, function (err, result) {
+                    response = result;
+                    res.send(JSON.parse(response));
+                });
+                //var response = xmlHandler.xmlToJson(null, req.body, null);
+            }
+        });
 
 };
